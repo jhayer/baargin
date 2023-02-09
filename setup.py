3e325d2c036aed61eea625b744c1ba87fc851d17
Jacques Dainat <jacques.dainat@gmail.com>
2023-02-09 21:38:26 +0100

    Update setup.py
    remove entry point change user

--- setup.py
+++ setup.py
@@ -11,16 +11,12 @@ setup(
 
     url='https://github.com/jhayer/nf-wgs_amr',
     download_url='https://github.com/jhayer/nf-wgs_amr/archive/v' + __version__ +'.tar.gz',
-    author='Martin Norling, Niclas Jareborg, Jacques Dainat',
+    author='Juliette Hayer, Jacques Dainat',
 
     license='GPL-3.0',
     packages=find_packages(),
 
-    install_requires=['pyyaml','requests','tarfile','python_version>="3.8.0"' ],
+    install_requires=['pyyaml', 'requests', 'tarfile', 'gzip', 'biopython>=1.78', 'numpy>=1.22', 'python_version>="3.8.0"' ],
     include_package_data=True,
-
-    entry_points={
-        'console_scripts': ['download_db = download_db:main',
-        ],
-    }
+    
 )

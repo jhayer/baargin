d17c2c53f9625b0e3c700026bf209cc46f392e15
Jacques Dainat <jacques.dainat@nbis.se>
2023-03-03 17:48:58 +0100

    add kraken mini db. Fix #8 and Fix #9

--- setup.py
+++ setup.py
@@ -4,19 +4,19 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='nf-wgs_amr',
+    name='baargin',
     __version__="0.1",
 
     description='xxx',
 
-    url='https://github.com/jhayer/nf-wgs_amr',
-    download_url='https://github.com/jhayer/nf-wgs_amr/archive/v' + __version__ +'.tar.gz',
+    url='https://github.com/jhayer/baargin',
+    #download_url='https://github.com/jhayer/baargin/archive/v' + __version__ +'.tar.gz',
     author='Juliette Hayer, Jacques Dainat',
 
     license='GPL-3.0',
     packages=find_packages(),
 
-    install_requires=['pyyaml', 'gitpython', 'requests', 'tarfile', 'gzip', 'biopython>=1.78', 'numpy>=1.22', 'python_version>="3.8.0"' ],
+    install_requires=[ 'pyyaml', 'gitpython', 'requests', 'tarfile', 'gzip', 'biopython>=1.78', 'numpy>=1.22', 'python_version>=3.8.0' ],
     include_package_data=True,
 
 )
